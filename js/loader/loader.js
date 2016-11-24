e950c7f79b23c7c6da28e3daec299d3ce48de0cc
Chang Liu <shampliu@gmail.com>
2016-11-24 14:51:42 -0800

    smoother loader

--- js/loader/loader.js
+++ js/loader/loader.js
@@ -4,36 +4,32 @@ var mask2 = document.getElementById('mask2');
 var deg1 = 90;
 var deg2 = -90;
 
+
 mask1.style.transform = 'rotate(' + deg1 + 'deg)';
 mask2.style.transform = 'rotate(' + deg2 + 'deg)';
 
 var progressInterval;
-var fps = 10;
+var fps = 60;
 
 document.addEventListener("DOMContentLoaded", function(event) {
     var percent = 0;
-    // progressInterval = setInterval(progress(mask1, mask2, percent), 1000);
-    progressInterval = setInterval(function() {
-        progress(mask1, mask2, percent);
+    progressInterval = setInterval(function() { // in order to use function with args
         percent++;
+        progress(mask1, mask2, percent);
     }, 1000/fps)
 });
 
 function progress(mask1, mask2, percent) {
-    if (percent < 50) {
-        deg1 += 360/100;
-        mask1.style.transform = 'rotate(' + deg1 + 'deg)';
-    } else {
-        if (percent == 50) {
-            convert(mask1); }
-        if (percent >= 100) {
-            clearInterval(progressInterval);
-            return;
-        }
+    deg1 += 360/100;
+    mask1.style.transform = 'rotate(' + deg1 + 'deg)';
 
-        deg2 += 360/100;
-        mask2.style.transform = 'rotate(' + deg2 + 'deg)';
+    if (percent >= 100) {
+        clearInterval(progressInterval);
+        return;
+    }
 
+    if (percent == 50) {
+        convert(mask2);
     }
 }
 
@@ -43,7 +39,6 @@ function convert(mask) {
         borderLeft: "3px solid red",
         borderRight: "3px solid red",
         transform: "rotate(90deg)",
-        background: "white",
-        transition: "none"
+        background: "white"
     })
 }
