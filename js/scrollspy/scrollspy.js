ceec624a4a7495e50f315b87059337acd53e16c5
Chang Liu <shampliu@gmail.com>
2017-01-17 20:26:56 -0800

    updates to scrollspy

--- js/scrollspy/scrollspy.js
+++ js/scrollspy/scrollspy.js
@@ -1,20 +1,24 @@
 (function() {
   var start = document.querySelectorAll('.article')[0].offsetTop;
   var end = document.querySelectorAll('.footer')[0].offsetTop;
-  var progress =document.querySelectorAll('.progress')[0];
+  var progress = document.querySelectorAll('.progress')[0];
+  var percent = document.getElementById('percent');
 
   window.onscroll = function() {
     console.log('scrolling');
     if (pageYOffset < start) {
       progress.style.width = "0%";
+      percent.innerHTML = "0%";
       return;
     }
 
     var perc = Math.round(((pageYOffset - start) / end) * 100);
     if (perc <= 100) {
       progress.style.width = perc + "%";
+      percent.innerHTML = perc + "%";
     } else {
       progress.style.width = "100%";
+      percent.innerHTML = "100%";
     }
 
   };
